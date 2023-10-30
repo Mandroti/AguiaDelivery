c6a51f24bcf8b9cf1c9b2e02261519aa005f3945
Mandroti <karolmandroti@outlook.com>
2023-10-30 15:30:31 -0300

    30-10

---
+++ static/js/dashboard.js
@@ -0,0 +1,255 @@
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+function editarCategoria() {
+    $('#modalEditarCategoria').modal('show');
+    }
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    let dropdownLinks = document.querySelectorAll('.tabela');
+
+    dropdownLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            let dropdownMenu = link.querySelector('.tabela-menu');
+            dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
+        });
+    });
+});
+
+var dados = [
+    {
+        nome: "Karolaine Mandroti",
+        horario: "03 out 10:42 (há poucos segundos)",
+        endereco: "Rua Minas Gerais, 64",
+        iconeEndereco: "fas fa-map-pin",
+        tipoEntrega: "Delivery",
+        iconeEntrega: "fas fa-motorcycle",
+        identificador: "card2",
+        tipoPagamento: "Cartão de Débito",
+        identificador: 1
+    },
+    {
+        nome: "Isabela Mandroti",
+        horario: "03 out 10:42 (há poucos segundos)",
+        endereco: "Rua Minas Gerais, 64",
+        iconeEndereco: "fas fa-map-pin",
+        tipoEntrega: "Delivery",
+        iconeEntrega: "fas fa-motorcycle",
+        identificador: "card2",
+        tipoPagamento: "Cartão de Débito",
+        identificador: 2
+    },
+    {
+        nome: "Miguel Mandroti",
+        horario: "03 out 10:42 (há poucos segundos)",
+        endereco: "Rua Minas Gerais, 64",
+        iconeEndereco: "fas fa-map-pin",
+        tipoEntrega: "Delivery",
+        iconeEntrega: "fas fa-motorcycle", //fa-location-arrow fa-map-marked-alt
+        identificador: "card3",
+        tipoPagamento: "Cartão de Débito",
+        identificador: 3
+    },
+];
+
+var pendentesContainer = document.getElementById('pendentes');
+
+for (var i = 0; i < dados.length; i++) {
+    var card = criarCard(dados[i]);
+    pendentesContainer.innerHTML += card;
+}
+
+function criarCard(dado) {
+
+    // var produtosHTML='';
+
+    // dado.produtos.forEach(function(produtos){
+    //     produtosHTML += `
+    //     <li>
+    //         <div style="display:flex">
+    //             <p>${produtos.qtde}X</p>    
+    //             <p>${produtos.nome}</p>  
+    //         </div>
+    //         <div>
+    //             <p>${produtos.complemento || ''}</p>
+    //         </div>
+    //     </li>
+    // `;
+    // });
+
+    var cardHTML = `
+        <div class="card" style="margin-right: -15px; margin-left: -15px">
+        <div style="padding: 5px">
+            <div style="display: flex;justify-content: space-between; "> 
+                <h4 style="margin: 10px 0px 0px 0px;">${dado.nome}</h4>   
+            
+                <div class="dropdown">
+                    <button class="mainmenubtn">
+                    <i class="fas fa-bars" style="margin-top: 16px;margin-left: auto; font-size: 24px;"></i> 
+                    </button>
+                    <div class="dropdown-child">
+                        <a href="#" onclick="proximaEtapa()">Pronto</a>     
+                        <a href="#" onclick="cancelarPedido()">Cancelar</a>                        
+                    </div>
+                </div>
+                            
+            </div>            
+            <p style="color: lightgray;">${dado.horario}</p>
+            <div style="display: flex;">
+                <i class="${dado.iconeEndereco}" style="margin-top: 5px;"></i>
+                <p style="color: gray; margin-left: 10px;">${dado.endereco}</p>
+            </div>
+            <div style="display: flex; align-items: center;">
+                <i class="${dado.iconeEntrega}" style="margin-bottom: 14px;"></i>
+                <p style="color: gray; margin-left: 10px;">${dado.tipoEntrega}</p>
+            </div>
+            <div style="display: flex; align-items: center;">
+                <i class="fas fa-money-bill-alt" style="margin-bottom: 14px;"></i>
+                <p style="color: gray; margin-left: 10px;">${dado.tipoPagamento}</p>
+            </div>
+            <div style="display: flex; flex-direction: column;">
+            <div style="color: gray; margin-left: 10px; text-decoration: underline; cursor: pointer;" onclick="toggleCard('${dado.identificador}')">Ver detalhes do pedido</div>
+                <div style="display:none;padding:10px; margin-top: 13px" id="${dado.identificador}">
+                    <div class="categoria">
+                    <div class="coluna">
+                        <h6 style="color: #ffbe33; text-align:start; font-size:18px; margin-left: 30px">Itens do Pedido</h6>
+                        <ul class="listaPedidos">
+                            <li>
+                                <div style="display:flex; margin-bottom: -15px">
+                                    <p>1X &emsp;</p>
+                                    <p>Hamburguer de Frango</p>
+                                </div>
+                                <div style="display: none;">
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Bacon&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Milho&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Calabresa&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Bacon&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Milho&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Calabresa&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Bacon&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Milho&ensp;</p>
+                                    <p style="display: inline; font-size: 12px;">1x&ensp; Calabresa&ensp;</p>
+                                </div>                            
+                            </li>    
+                            <li>
+                                <div style="display:flex;  margin-bottom: -15px">
+                                    <p>1X &emsp;</p>
+                                    <p>Pizza Brócolis</p>
+                                </div>
+                            </li>        
+                            <li>
+                                <div style="display:flex; margin-bottom: -15px">
+                                    <p>3X &emsp;</p>
+                                    <p>Pastel de Queijo</p>
+                                </div>
+                            </li>      
+                            <li>
+                                <div style="display:flex;  margin-bottom: -15px">
+                                    <p>1X &emsp;</p>
+                                    <p>Pizza Brócolis</p>
+                                </div>
+                            </li>        
+                            <li>
+                                <div style="display:flex">
+                                    <p>3X &emsp;</p>
+                                    <p>Pastel de Queijo</p>
+                                </div>
+                            </li>      
+                        </ul>
+                    </div>                   
+                </div>
+                </div>
+            </div>
+        </div>           
+        </div>
+    `;
+
+    return cardHTML;
+}
+
+function toggleCard(cardId) {
+    var cardBody = document.getElementById(cardId);
+    if (cardBody.style.display === 'none') {
+        cardBody.style.display = 'block';
+    } else {
+        cardBody.style.display = 'none';
+    }
+}
+// var producaoContainer = document.getElementById('producao');
+
+// for (var i = 0; i < dados.length; i++) {
+//     var card = criarCard(dados[i]);
+//     producaoContainer.innerHTML += card;
+// }
+
+// var disponivelContainer = document.getElementById('disponivel');
+
+// for (var i = 0; i < dados.length; i++) {
+//     var card = criarCard(dados[i]);
+//     disponivelContainer.innerHTML += card;
+// }
+
+
+// function passarFase(){
+//     const modal = document.getElementById('myModal');
+//     modal.style.display = 'block';
+// }
+
+// function fecharModal() {
+//     const modal = document.getElementById('myModal');
+//     modal.style.display = 'none';
+// }
+
+function pedidosCliente(){
+    
+}
+
+function cancelarPedido(){
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+
+    // Adicionando uma função para fechar o modal
+    const fecharModal = document.getElementById('fecharModal');
+    fecharModal.addEventListener('click', function(){
+        modal.style.display = 'none';
+    });
+
+    const fechar = document.getElementById('fechar');
+    fechar.addEventListener('click', function(){
+        modal.style.display = 'none';
+    });
+}
+
+function confirmarCancelarProduto(){
+   
+}
+
+function proximaEtapa(){
+    const modal = document.getElementById('myModalEtapa');
+    modal.style.display = 'block';
+
+    // Adicionando uma função para fechar o modal
+    const fecharModal = document.getElementById('fecharModalEtapa');
+    fecharModal.addEventListener('click', function(){
+        modal.style.display = 'none';
+    });
+
+    const fechar = document.getElementById('fecharEtapa');
+    fechar.addEventListener('click', function(){
+        modal.style.display = 'none';
+    });
+}
---
+++ static/js/login.js
@@ -0,0 +1,445 @@
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    
+//MASCARAS DE FORMATAÇÃO INPUTS
+function applyPhoneMask(event) {
+    let input = event.target;
+    let value = input.value;
+    if (!value) return "";
+    value = value.replace(/\D/g,'');
+    value = value.replace(/(\d{2})(\d)/,"($1) $2");
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2");
+    input.value = value;
+}
+
+const applyZipCodeMask = (event) => {
+    let input = event.target;
+    let value = input.value;
+    
+    if (!value) return "";
+    
+    value = value.replace(/\D/g,'');
+    value = value.replace(/(\d{5})(\d)/,'$1-$2');
+    
+    input.value = value;
+}
+
+function applyCNPJMask(event) {
+    let input = event.target;
+    let value = input.value;
+    value = value.replace(/\D/g,"");                           
+    value = value.replace(/^(\d{2})(\d)/,"$1.$2");              
+    value = value.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3"); 
+    value = value.replace(/\.(\d{3})(\d)/,".$1/$2");          
+    value = value.replace(/(\d{4})(\d)/,"$1-$2");             
+    input.value = value;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cepInput = document.getElementById("inputCep");
+
+    cepInput.addEventListener('blur', function() {
+        const cepValue = cepInput.value.replace(/[^0-9]+/, '');
+        if (cepValue.length === 8) {
+            fetchCEP(cepValue);
+        }
+    });
+});
+
+function fetchCEP(cepValue) {
+    const url = `https://viacep.com.br/ws/${cepValue}/json/`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            if (json.logradouro) {
+                document.getElementById('inputLog').value = json.logradouro;
+                document.getElementById('inputBairro').value = json.bairro;
+                document.getElementById('inputCit').value = json.localidade;
+                document.getElementById('inputUf').value = json.uf;
+            }
+        });
+}
+
+function checkTelefone(event)
+{
+    var value = event.target.value;
+
+    var phoneRegex = /^\d{11,}$/;
+
+    if (phoneRegex.test(value)) {
+        applyPhoneMask(event);
+    }
+}
+
+
+
+//ANIMAÇÕES
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+
+function exibeSenha(inputId) {
+    var eyeIcon = document.querySelector('#' + inputId + ' + .input-group-append .toggle-password');
+    var passwordInput = document.getElementById(inputId);
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.querySelector('i').classList.remove('bi-eye-slash');
+        eyeIcon.querySelector('i').classList.add('bi-eye');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.querySelector('i').classList.remove('bi-eye');
+        eyeIcon.querySelector('i').classList.add('bi-eye-slash');
+    }
+}
+
+
+
+
+
+
+function cadastrarEstabelecimento(){
+    event.preventDefault();   
+    window.location.href = "aguardandoEmail.html";
+}
+function TestarAPI()
+{    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("http://aguiadelivery.com.br:6060/api/Autoriza", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));     
+        
+    event.preventDefault();          
+}
+
+
+
+//LOGINS E RECUPERAÇÃO DE SENHA
+function meuLogin()
+{
+    //UM VAI PARA O CARDAPIO E OUTRO PARA A DASHBOARD
+    const contato = document.getElementById('username').value;
+    const senha = document.getElementById('password').value;
+    
+    const url = `https://localhost:7221/api/Usuario/Login?contato=${contato}&senha=${senha}`;
+    const dados = {
+        contato: contato,
+        senha: senha
+    };
+
+    fetch(url, { 
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },        
+    })
+    .then(response => {
+        if (!response.ok) {
+          window.location.href = "registraCliente.html"
+        }
+        else
+            window.location.href = "dashboard.html"
+        return response.json();})
+    .then(data =>{
+     })
+    .catch(error => console.error('Erro:', error));
+    event.preventDefault();
+}    
+
+function recuperarSenha()
+{
+    document.getElementById('naoTelefone').style.display = 'none';
+    var telefone = document.getElementById('username').value;
+  
+    var regex = /^\([1-9]{2}\) 9[0-9]{4}-[0-9]{4}$/;
+
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(regexEmail.test(telefone))
+    {
+        event.preventDefault();   
+        document.getElementById('mudarSenha').style.display = 'block';
+    }
+    else{
+        if (regex.test(telefone)) {
+            event.preventDefault();   
+            document.getElementById('mudarSenha').style.display = 'block';
+    
+            //PUT no cadastro do cliente e encaminha para o cardapio
+        } else {
+            event.preventDefault();   
+            document.getElementById('naoTelefone').style.display = 'block';
+        }
+    }   
+   
+}
+
+
+
+//FETCH ESTABELECIMENTO
+function registraEstabelecimento()
+{
+       var nome = document.getElementById("inputNome").value;       
+       var cnpj_Cpf = document.getElementById("inputCnpj").value;
+       var cnpj_Cpf = cnpj_Cpf.replace(/\D/g,'');  
+       var ie_Rg = ""; //nao tem no formulario        
+       var nomeFantasia = ""; //nao tem no formulario       
+       var telefone = document.getElementById("inputTelefone").value;   
+       telefone = telefone.replace(/\D/g,'');
+       var celular = document.getElementById("inputTelefone").value;    
+       celular = celular.replace(/\D/g,'');
+       var emailPrincipal = document.getElementById("inputEmail").value;      
+       var logradouro = "teste";       
+       var bairro = "teste";      
+       var cep = "teste";                  
+       var numero = "";      
+       var cidade_IBGE = "111"; //nao tem no formulario       
+       var uf = "SP";      
+       var senha = document.getElementById("password").value;       
+       var confirmaSenha = document.getElementById("inputConfirmar").value;       
+       var observacao = "teste"; //nao tem no formulario        
+       var logo = ""; //nao tem no formulario       
+       var active = true; //nao tem no formulario
+   
+        const dados = {
+            nome: nome, cnpj_Cpf: cnpj_Cpf, ie_Rg: ie_Rg, nomeFantasia: nomeFantasia,
+            telefone: telefone, celular: celular, emailPrincipal: emailPrincipal, logradouro: logradouro,
+            bairro: bairro, cep: cep, numero: numero, cidade_IBGE: cidade_IBGE, uf: uf, senha:senha, 
+            observacao: observacao, logo: logo, active: active
+        };
+
+        if(senha == confirmaSenha)
+        {
+            fetch('http://aguiadelivery.com.br:6060/api/Estabelecimento', {             
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',               
+                },
+                body: JSON.stringify(dados)           
+            })
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));     
+            event.preventDefault();   
+        }
+        else{        
+              alert("Caiu no else");            
+        }        
+}
+
+function buscarEstabelecimentoId(id)
+{
+    fetch(`http://aguiadelivery.com.br:6060/api/Estabelecimento/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Estabelecimento ID:', data);
+    })
+    .catch(error => console.error('Erro ao buscar por ID:', error));
+
+    event.preventDefault();
+}
+
+function removerEstabelecimento(id)
+{
+    fetch(`http://aguiadelivery.com.br:6060/api/Estabelecimento/${id}`, {
+        method: 'DELETE'
+    })
+    .then(data => {
+        console.log('Estabelecimento ID:', data);
+    })
+    .catch(error => console.error('Erro ao deletar o ID:', error));
+
+    event.preventDefault();
+}
+
+function alterarEstabelecimentoId()
+{
+    var nome = document.getElementById("inputNome").value;       
+    var cnpj_Cpf = document.getElementById("inputCnpj").value;
+    var cnpj_Cpf = cnpj_Cpf.replace(/\D/g,'');  
+    var ie_Rg = ""; //nao tem no formulario        
+    var nomeFantasia = ""; //nao tem no formulario       
+    var telefone = document.getElementById("inputTelefone").value;   
+    telefone = telefone.replace(/\D/g,'');
+    var celular = document.getElementById("inputTelefone").value;    
+    celular = celular.replace(/\D/g,'');
+    var emailPrincipal = document.getElementById("inputEmail").value;      
+    var logradouro = "teste";       
+    var bairro = "teste";      
+    var cep = "teste";                  
+    var numero = "";      
+    var cidade_IBGE = "111"; //nao tem no formulario       
+    var uf = "SP";      
+    var senha = document.getElementById("password").value;       
+    var confirmaSenha = document.getElementById("inputConfirmar").value;       
+    var observacao = "teste"; //nao tem no formulario        
+    var logo = ""; //nao tem no formulario       
+    var active = true; //nao tem no formulario
+
+     const dados = {
+         nome: nome, cnpj_Cpf: cnpj_Cpf, ie_Rg: ie_Rg, nomeFantasia: nomeFantasia,
+         telefone: telefone, celular: celular, emailPrincipal: emailPrincipal, logradouro: logradouro,
+         bairro: bairro, cep: cep, numero: numero, cidade_IBGE: cidade_IBGE, uf: uf, senha:senha, 
+         observacao: observacao, logo: logo, active: active
+     };
+
+
+    fetch('http://aguiadelivery.com.br:6060/api/Estabelecimento', {             
+        method: 'PUT',
+        headers:{
+            'Content-Type': 'application/json',               
+        },
+        body: JSON.stringify(dados)           
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao atualizar estabelecimento');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Estabelecimento atualizado com sucesso:', data);
+      })
+      .catch(error => console.error('Erro:', error));
+      event.preventDefault();     
+}
+
+
+
+
+//FETCH CLIENTE
+function registraCliente()
+{
+        var nome = document.getElementById('inputNome').value;      
+        var telefone = document.getElementById('inputContato').value;
+        telefone = telefone.replace(/\D/g,'');       
+        var active = true;      
+        var logradouro = document.getElementById('inputLog').value;        
+        var bairro = document.getElementById('inputBairro').value;      
+        var cep = document.getElementById('inputCep').value;
+        cep = cep.replace(/\D/g,'');    
+        var numero = document.getElementById('inputNum').value;       
+        var cidade_IBGE = "111";        
+        var uf = document.getElementById('inputUf').value;          
+        var senha = document.getElementById('password').value;        
+        var confirmaSenha = document.getElementById('inputConfirmar').value;
+
+        const dados = {
+            nome: nome, telefone: telefone, active: active, logradouro: logradouro,
+            bairro: bairro, cep: cep, numero: numero, cidade_IBGE: cidade_IBGE, uf: uf,
+            senha:senha
+        };       
+        
+
+        if(senha == confirmaSenha)
+        {
+            fetch('http://aguiadelivery.com.br:6060/api/Consumidor', {             
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',               
+                },
+                body: JSON.stringify(dados)           
+            })
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.error('Erro:', error));        
+            event.preventDefault();
+     
+        }
+        else{          
+            event.preventDefault();   
+            document.getElementById('senhaDiferenteAlert').style.display = 'block';
+        }        
+}
+
+function buscarClienteId(id)
+{
+    fetch(`http://aguiadelivery.com.br:6060/api/Consumidor/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Cliente ID:', data);
+    })
+    .catch(error => console.error('Erro ao buscar por ID:', error));
+
+    event.preventDefault();   
+}
+
+function removerCliente(id)
+{
+    fetch(`http://aguiadelivery.com.br:6060/api/Consumidor/${id}`, {
+        method: 'DELETE'
+    })
+    .then(data => {
+        console.log('Cliente ID:', data);
+    })
+    .catch(error => console.error('Erro ao deletar o ID:', error));
+
+    event.preventDefault();   
+}
+
+function alterarClienteId()
+{
+    var nome = document.getElementById('inputNome').value;      
+    var telefone = document.getElementById('inputContato').value;
+    telefone = telefone.replace(/\D/g,'');       
+    var active = true;      
+    var logradouro = document.getElementById('inputLog').value;        
+    var bairro = document.getElementById('inputBairro').value;      
+    var cep = document.getElementById('inputCep').value;
+    cep = cep.replace(/\D/g,'');    
+    var numero = document.getElementById('inputNum').value;       
+    var cidade_IBGE = "111";        
+    var uf = document.getElementById('inputUf').value;          
+    var senha = document.getElementById('password').value;        
+    var confirmaSenha = document.getElementById('inputConfirmar').value;
+
+    const dados = {
+        nome: nome, telefone: telefone, active: active, logradouro: logradouro,
+        bairro: bairro, cep: cep, numero: numero, cidade_IBGE: cidade_IBGE, uf: uf,
+        senha:senha
+    };       
+
+    fetch('http://aguiadelivery.com.br:6060/api/Consumidor', {             
+        method: 'PUT',
+        headers:{
+            'Content-Type': 'application/json',               
+        },
+        body: JSON.stringify(dados)           
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao atualizar usuário');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Usuário atualizado com sucesso:', data);
+      })
+      .catch(error => console.error('Erro:', error));
+      event.preventDefault();     
+}
