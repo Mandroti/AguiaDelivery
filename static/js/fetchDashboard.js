e232efce02273d1edc6ddd50f382def6d9d3fb57
Mandroti <karolmandroti@outlook.com>
2023-11-13 15:24:57 -0300

    13-11

--- static/js/fetchDashboard.js
+++ static/js/fetchDashboard.js
@@ -991,6 +991,12 @@ function editarProduto(){
 
 }
 
+var categoriaSelecionada = 0;
+function pegarCat(categoria){
+    categoriaSelecionada = categoria;
+    listarComplementos();
+}
+
 function fetchCategoria() {
     var id = localStorage.getItem("id");
     var token = localStorage.getItem("token");
@@ -1011,7 +1017,7 @@ function fetchCategoria() {
 
 
             data.forEach(function (data) {
-                selectCategoria.innerHTML += `<option value="${data.id}">${data.nome}</option>`;
+                selectCategoria.innerHTML += `<option value="${data.categoriaId}" onclick="pegarCat(${data.categoriaId})">${data.nome}</option>`;
             });
 
             
@@ -1116,7 +1122,6 @@ function carregarProd(dado){
             <th scope="row">${dado.produtoId}</th>
             <td>${dado.nome}</td>
             <td>${dado.preco}</td>
-            <td>Ativo</td>
             <td>
                 <div>
                     <a class="btn table-action" href="#">
@@ -1130,137 +1135,183 @@ function carregarProd(dado){
 
     return row;
 }
-// function listarComplementos(){
-//     const id = localStorage.getItem("id");
-//     const token = localStorage.getItem("token");
-//     fetch(apiUrl+`/api/Complemento/Estabelecimento/${id}`, { 
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}` 
-//         }
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         var tabela = 
-//                 `<div class="col-md-4">
-//                     <div>
-//                         <input type="checkbox" id="${data.id}">
-//                         <label for="${data.id}">${data.nome}</label>
-//                     </div>                                                      
-//                 </div>`;
 
-//         document.getElementById('listaComplementos').innerHTML = tabela;
-//     })
-//     .catch(error => console.error('Erro:', error));
+function listarComplementos(){
+    const token = localStorage.getItem("token");
+    fetch(apiUrl+`/api/Complemento?CategoriaId=${categoriaSelecionada}`, { 
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}` 
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('listaComplementos').innerHTML = '';
 
-//     event.preventDefault();
-// }
+        for (let i = 0; i < data.length; i++) {
+            
+            var tabela = 
+                `<div class="col-md-4">
+                    <div>
+                        <input type="checkbox" id="${data[i].complementoId}">
+                        <label for="${data[i].complementoId}">${data[i].nome}</label>
+                    </div>                                                      
+                </div>`;
 
-// var produtos = document.getElementById('tabelaProdutos');
+            
+            document.getElementById('listaComplementos').innerHTML += tabela;
+        }
+    })
+    .catch(error => console.error('Erro:', error));
 
-// function buscarProdutos()
-// {
-//     const token = localStorage.getItem("token");
-//     const id = localStorage.getItem("id");
-//     fetch(apiUrl+`/api/Produto/Estabelecimento/${id}`, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`  
-//         }
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         var tabela = `<table class="table table-striped">
-//                         <thead>
-//                             <tr>
-//                                 <th scope="col">#</th>
-//                                 <th scope="col">Nome</th>
-//                                 <th scope="col">Valor</th>
-//                                 <th scope="col">Situação</th>
-//                                 <th scope="col">Opções</th>
-//                             </tr>
-//                         </thead>
-//                         <tbody>`;
+    event.preventDefault();
+}
 
-//         for (var i = 0; i < data.length; i++) {
-//             var card = carregarProdutos(data[i]);
-//             tabela += card;
-//         }
+function criarProduto(){
 
-//         tabela += `</tbody></table>`;
-
-//         produtos.innerHTML = tabela;
-//     })
-//     .catch(error => console.error('Erro:', error));
-
-//     event.preventDefault();
-// }
-
-// function carregarProdutos(dado){
-//     var row = `
-//         <tr>
-//             <th scope="row">${dado.produtoId}</th>
-//             <td>${dado.nome}</td>
-//             <td>${dado.preco}</td>
-//             <td>Ativo</td>
-//             <td>
-//                 <div>
-//                     <a class="btn table-action" href="#">
-//                         <i class="action-icon fas fa-edit" onclick="editarProduto(${dado.produtoId})"></i>
-//                         <i class="action-icon fas fa-trash" onclick="excluirProduto(${dado.produtoId})"></i>
-//                     </a>                         
-//                 </div>
-//             </td>
-//         </tr>
-//     `;
-
-//     return row;
-// }
-
-// function criarProduto(){
-
-//     const token = localStorage.getItem("token");
-
-//     var nome = document.getElementById('inputNome').value; 
-//     var valor = document.getElementById('inputValor').value;
-//     var categoria = document.getElementById('inputNome').value; 
-//     var descricao = document.getElementById('inputValor').value;
-//     var limite = document.getElementById('inputNome').value; 
-//     var observacao = document.getElementById('inputValor').value;
-//     var imagem = document.getElementById('inputNome').value; 
-//     var complemento = document.getElementById('inputValor').value;
-//     var variacao = document.getElementById('inputNome').value; 
+    const token = localStorage.getItem("token");
+    const id = localStorage.getItem("id");
 
 
-//     const dados = {
-//         nome: nome, precoVenda: valor
-//     };       
+    var nome = document.getElementById('inputNome').value; 
+    var valor = document.getElementById('inputValor').value;
+    var categoria = categoriaSelecionada;
+    var descricao = document.getElementById('inputDescricao').value;
+    var imagem = document.getElementById('inputImagem').value;    
 
-//     fetch(apiUrl+'/api/Complemento/',{             
-//         method: 'POST',
-//         headers:{
-//             'Content-Type': 'application/json',    
-//             'Authorization': `Bearer ${token}`             
-//         },
-//         body: JSON.stringify(dados)           
-//     })
-//     .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Erro ao cadastrar complemento');
-//         }
-//         else{
-//             window.location.href = "buscarComplemento.html"
-//         }
-//         return response.text();
-//       })
-//       .then(data => {
-//         console.log('Complemento cadastrado com sucesso:', data);
-//       })
-//       .catch(error => console.error('Erro:', error));
-//       event.preventDefault();  
-// }
 
+    const dados = {
+        estabelecimentoId: id, nome: nome, descricao: descricao, imagem: imagem, 
+        preco: valor, categoriaId: categoria, active: true
+    };       
+
+    fetch(apiUrl+'/api/Produto',{             
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',    
+            'Authorization': `Bearer ${token}`             
+        },
+        body: JSON.stringify(dados)           
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao cadastrar produto');
+        }
+        else{
+            // window.location.href = "buscarProduto.html"
+            console.log(response)
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log('Produto cadastrado com sucesso:', data);
+      })
+      .catch(error => console.error('Erro:', error));
+      event.preventDefault();  
+}
+
+function criaVariacao(){
+    const token = localStorage.getItem("token");
+   
+    var titulo = document.getElementById('inputTitulo').value; 
+    var categoriaId = categoriaSelecionada;
+      
+    const dados = {
+        titulo: titulo, categoriaId: categoriaId, active: true
+    };           
+
+    fetch(apiUrl+'/api/Variacao/Grupo',{             
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',    
+            'Authorization': `Bearer ${token}`             
+        },
+        body: JSON.stringify(dados)           
+    })
+    .then(response => {
+        console.log(response.status); 
+        if (!response.ok) {
+          throw new Error('Erro ao cadastrar produto');
+        }
+        else{
+            // window.location.href = "buscarProduto.html"
+            console.log(response)
+            adicionaVariacao();
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log('Produto cadastrado com sucesso:', data);
+      })
+      .catch(error => console.error('Erro:', error));
+      event.preventDefault();  
+}
+
+function adicionaVariacao() {
+    const token = localStorage.getItem("token");
+    fetch(apiUrl + '/api/Variacao/Grupo?CategoriaId='+categoriaSelecionada, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById('variacao').innerHTML = ''; 
+
+            for (let i = 0; i < data.length; i++) {
+                var tabela = `<div class="input-group mb-3"  style="margin-top: 10px;">
+                                <label type="text" class="form-control">${data[i].nome}</label>
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-success" type="button">+</button>
+                                </div>
+                            </div> `;
+
+                document.getElementById('variacao').innerHTML += tabela; 
+            }
+        })
+        .catch(error => console.error('Erro:', error));
+}
+
+
+function carregarVariacao(dado){
+    const token = localStorage.getItem("token");
+    fetch(apiUrl+``, { //procura pelo grupo id
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}` 
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('variacao').innerHTML = '';
+
+        for (let i = 0; i < data.length; i++) {           
+            var row = `<table class="table table-striped">                                    
+                            <tbody>
+                                <tr>
+                                    <th scope="row">${dado.variacaoId}</th>
+                                    <td>${dado.name}</td>                                            
+                                    <td>
+                                        <div>
+                                            <a class="btn table-action" href="#">                                                       
+                                                <i class="action-icon fas fa-trash"></i>
+                                            </a>                         
+                                        </div>
+                                    </td>
+                                </tr>                        
+                            </tbody>
+                        </table>`;
+            return row;
+        }
+    })
+    .catch(error => console.error('Erro:', error));
+
+    event.preventDefault();
+}
